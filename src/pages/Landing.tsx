1ba82c11932affd4d4c410fdd345f9c0e121d7d9
Higor Moroni <higor.moroni@prodigious.com>
2021-01-04 15:09:33 -0300

    Router funcionando

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import '../styles/pages/landing.css';
 
@@ -10,7 +11,7 @@ function Landing() {
     <div id="page-landing">
       <div className="content-wrapper">
         <img src={logoimg} alt="Happy" />
-
+ 
         <main>
           <h1>Leve felicidade para o mundo</h1>
           <p>Visite orfanatos e mude o dia de muitas crianças.</p>
@@ -21,9 +22,9 @@ function Landing() {
           <span>Santa Catarina</span>
         </div>
 
-        <a href="" className="enter-app">
+        <Link to="/app" className="enter-app">
           <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
-        </a>
+        </Link>
       </div>
     </div>
   );
