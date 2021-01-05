f821d3f8b2870f4cb9154e679368030bc51aa658
Higor Moroni <higor.moroni@prodigious.com>
2021-01-05 10:38:42 -0300

    Adicionando api do Mapbox

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { MapContainer, TileLayer } from 'react-leaflet'
 
 import '../styles/pages/orphanages-map.scss';
+import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
@@ -21,7 +23,15 @@ function OrphanagesMap() {
         </footer>
       </aside>
 
-      <div></div>
+      <MapContainer
+        center={[-23.9580611,-46.3532323]}
+        zoom={14}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      </MapContainer>
 
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
